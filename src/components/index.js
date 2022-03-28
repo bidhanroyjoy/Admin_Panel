cfc5aa9869e67986e28c0178b9ced885ae062471
bidhanroyjoy <bidhan19994@gmail.com>
2022-03-29 01:02:08 +0600

    Layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from './navbar/Navbar';
 import Sidebar from './sidebar/Sidebar';
+import Widget from './widget/Widget';
 
 export {
     Navbar,
     Sidebar,
+    Widget,
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar,Sidebar,Widget } from '../../components'
 import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Sidebar />
+      <div className='homeContainer'>
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+      </div>
+    </div>
   )
 }
 
