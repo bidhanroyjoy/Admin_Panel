ffd39ffe172cd0e00fa173748158c6fa7fa9ae20
bidhanroyjoy <bidhan19994@gmail.com>
2022-03-28 20:38:09 +0600

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Login,Home, List, Single, New } from './pages';
+// import {  } from './components';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />}/>
+            <Route path="login" element={<Login />} />
+          </Route>
+          <Route path='users'>
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} title='Add New User'/>
+          </Route>
+          <Route path='products'>
+            <Route index element={<List />} />
+            <Route path=':productId' element={<Single />} />
+            <Route path='new' element={<New />} title='Add New Product'/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
